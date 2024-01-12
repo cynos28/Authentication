470446b31ac47360107e0ef67cb3a40828186a77
Cynos28 <sendtocynos@gmail.com>
2024-01-12 18:33:57 +0530

    header css

--- client/src/components/header/header.js
+++ client/src/components/header/header.js
@@ -1,34 +1,59 @@
 import React from 'react'
 import "./header.css";
 import { FaHotel } from "react-icons/fa6";
-import { FaUserCircle } from "react-icons/fa";
+import { FaCircleUser } from "react-icons/fa6";
+import { Link, NavLink } from 'react-router-dom';
 
+const activeLink =({isActive}) => (isActive ? "active" : "");
 
 function header() {
-  return (
-    <header className='header'>
-        <nav>
-            <div className="logo">
-            <FaHotel size={35} color='#17D7A0'/> <span>PrimeLodge</span>
-            </div>
+    return (
+        <header className='header'>
+            <nav>
+                <div className="logo">
+                    <FaHotel size={35} color='#17D7A0' /> <span>PrimeLodge</span>
+                </div>
 
-            <div className="home-links">
-                <li className='user'>
-                    <FaUserCircle size={20}/>
-                    <p className="--color-black">
-                        Hi Shehan |
-                    </p>
+                <div className="home-links">
+                    
+                <li>
+                <Link to ="/login" >
+                        <button class="button-login">
+                            <svg class="svg-icon" fill="none" height="20" viewBox="0 0 20 20" width="20" xmlns="http://www.w3.org/2000/svg"><g stroke="#206a5d" stroke-linecap="round" stroke-width="1.5"><path d="m3.33337 10.8333c0 3.6819 2.98477 6.6667 6.66663 6.6667 3.682 0 6.6667-2.9848 6.6667-6.6667 0-3.68188-2.9847-6.66664-6.6667-6.66664-1.29938 0-2.51191.37174-3.5371 1.01468"></path><path d="m7.69867 1.58163-1.44987 3.28435c-.18587.42104.00478.91303.42582 1.0989l3.28438 1.44986"></path></g></svg>
+                            <span class="lable"> Login
+                            </span>
+                        </button>
+                        </Link>
+                    </li>
 
 
-                </li>
+                    <li className='user'>
+                        <FaCircleUser size={30} />
+                        <p className="user-name">
+                            Hi Shehan |
+                        </p>
+
+                    </li>
+
+                    <li className='profile-button'>
+
+                        <NavLink to = "/profile" className={activeLink} > Profile </NavLink>
+                    </li>
+                    
+                    <li>
+                            <button className='button-logout'>
+                                Logout
+                            </button>
+
+                    </li>
 
 
-            </div>
+                </div>
 
-        </nav>
+            </nav>
 
-    </header>
-  )
+        </header>
+    )
 }
 
 export default header
